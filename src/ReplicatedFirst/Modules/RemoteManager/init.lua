5aedc971e61098030a491508208335420d00a19e
RainyLofi <krowlands333@gmail.com>
2022-01-11 20:17:20 +0000

    Check the script attempting to get the remote instance

--- src/ReplicatedFirst/Modules/RemoteManager/init.lua
+++ src/ReplicatedFirst/Modules/RemoteManager/init.lua
@@ -14,7 +14,7 @@ local Module = {}
 
 Module.RemoteStorage = ReplicatedStorage:FindFirstChild('Remotes')
 if IsServer and not Module.RemoteStorage then
-    Module.RemoteStorage = Instance.new('Folder', ReplicatedStorage)
+	Module.RemoteStorage = Instance.new('Folder', ReplicatedStorage)
 	Module.RemoteStorage.Name = 'Remotes'
 elseif not IsServer then
 	Module.RemoteStorage = ReplicatedStorage:WaitForChild('Remotes')
@@ -23,7 +23,7 @@ end
 Module.BindParent = IsServer and game:GetService('ServerStorage') or ReplicatedStorage
 Module.BindStorage = Module.BindParent:FindFirstChild('Bindables')
 if not Module.BindStorage then
-    Module.BindStorage = Instance.new('Folder', Module.BindParent)
+	Module.BindStorage = Instance.new('Folder', Module.BindParent)
 	Module.BindStorage.Name = 'Bindables'
 end
 
@@ -31,13 +31,10 @@ local RemoteInstance = require(script:WaitForChild('RemoteInstance'))
 
 -----------------------------------------------
 
-Module.Encrypt = function(data: string, edit: boolean)
-	local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-	if edit then b = '␍␛␙␆␂␋␔␕␜␝␒␑␈␏␍␂␂␒␐␏␛␍␂␐␍␊␕␍␃␘␃␞␕␄␋␁␖␕␞␎␓␎␀␋␒␋␐␅␋␙␔␒␓␔␝␓␍␚␡␆␎␗␗␆' end
+Module.Encode = function(data: string)
+	local b = '␍␛␙␆␂␋␔␕␜␝␒␑␈␏␍␂␂␒␐␏␛␍␂␐␍␊␕␍␃␘␃␞␕␄␋␁␖␕␞␎␓␎␀␋␒␋␐␅␋␙␔␒␓␔␝␓␍␚␡␆␎␗␗␆'
 
-	if not RunService:IsStudio() then
-		data = game.JobId .. data
-	end
+	if not RunService:IsStudio() then data = game.JobId .. data end
 
 	return ((data:gsub('.', function(x)
 		local r,b='',x:byte()
@@ -54,29 +51,35 @@ end
 -----------------------------------------------
 
 local Types = { -- classname -> remote/not, event/function
-    ['RemoteEvent'] = {true, true},
-    ['RemoteFunction'] = {true, false},
-    ['BindableEvent'] = {false, true},
-    ['BindableFunction'] = {false, false}
+	['RemoteEvent'] = {true, true},
+	['RemoteFunction'] = {true, false},
+	['BindableEvent'] = {false, true},
+	['BindableFunction'] = {false, false}
 }
 
 Module.Get = function(self, Type: string, Name: string)
-    local TypeData = Types[Type]
-    if not TypeData then return end
+	
+	-- check to see if the script getting the remote is valid
+	-- can be bypassed using syn.secure_call however this is better than nothing
+	local _, Source = pcall(function() return getfenv(2).script end)
+	if not Source or not Source.Parent then return Module:Get(true, 'DONT_EXPLOIT!') end -- should crash
+	
+	local TypeData = Types[Type]
+	if not TypeData then return end
 
-    local IsRemote = table.unpack(TypeData)
+	local IsRemote = table.unpack(TypeData)
 
-    local EncryptedName = Module.Encrypt(Name)
-    local Parent = IsRemote and Module.RemoteStorage or Module.BindStorage
+	local EncodedName = Module.Encode(Name)
+	local Parent = IsRemote and Module.RemoteStorage or Module.BindStorage
 
-    local Remote = Parent:FindFirstChild(EncryptedName)
-    if not Remote then
-	if not IsServer and IsRemote then
-	    Remote = Parent:WaitForChild(EncryptedName)
-        end
-    end
+	local Remote = Parent:FindFirstChild(EncodedName)
+	if not Remote then
+		if not IsServer and IsRemote then -- client should wait for any events that are remote
+			Remote = Parent:WaitForChild(EncodedName)
+		end
+	end
 
-    return RemoteInstance.new(EncryptedName, TypeData, Type, Parent)
+	return RemoteInstance.new(EncodedName, TypeData, Type, Parent)
 end
 
 return Module
